73d1739719067defe980dbe80cf51915b5d6cf15
SwapnilSomwanshi89 <swapnil.somwanshi@zconsolutions.com>
2015-11-13 15:23:52 +0530

    first git upload

---
+++ Examples/src/com/zcon/examples/Vehicle.java
@@ -0,0 +1,20 @@
+package com.zcon.examples;
+
+class Vehicle1{
+	int x = 10;
+	  void run(int a, float b){
+	       
+	       System.out.println("Vehicle1 is running"+a+"  B: "+b);}
+	  }
+	  class Vehicle extends Vehicle1{
+	  void run(float b, int a,String ab){
+	        
+	       System.out.println("Vehicle is running"+a+"  B: "+b);}
+
+	  public static void main(String args[]){
+	  Vehicle obj = new Vehicle();
+	  obj.run(10,50);
+	  System.out.println(obj.x);
+	  }
+	}
+
